bbf278f152fff1bfa773a5d6389da76522a85d70
Hana Dawson <stubbornunicorn95@gmail.com>
2021-03-15 11:56:18 -0600

    Added Image entity for database.

--- src/main/java/edu/cnm/deepdive/galleryservice/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/galleryservice/model/entity/User.java
@@ -2,12 +2,18 @@ package edu.cnm.deepdive.galleryservice.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,28 +33,28 @@ import org.springframework.lang.NonNull;
 public class User {
 
   @NonNull
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "contributor", cascade = CascadeType.ALL)
+  @OrderBy("created DESC")
+  private final List<Image> images = new LinkedList<>();
+  @NonNull
   @Id
   @GeneratedValue(generator = "uuid")
   @GenericGenerator(name = "uuid2", strategy = "uuid2")
   @Column(name = "user_id", nullable = false, updatable = false, columnDefinition = "CHAR(16) FOR BIT DATA")
   private UUID id;
-
   @NonNull
   @CreationTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false, updatable = false)
   private Date created;
-
   @NonNull
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false)
   private Date connected;
-
   @NonNull
   @JsonIgnore
   @Column(nullable = false, updatable = false,unique = true)
   private String oauthKey;
-
   @NonNull
   @Column(nullable = false, unique = true)
   private String displayName;
@@ -88,5 +94,10 @@ public class User {
 
   public void setDisplayName(@NonNull String displayName) {
     this.displayName = displayName;
+  }
+
+  @NonNull
+  public List<Image> getImages() {
+    return images;
   }
 }
