e39a13af155c53e17ff2f0dd345018629e274382
Hana Dawson <stubbornunicorn95@gmail.com>
2021-03-16 09:57:34 -0600

    Started implementation of href.

--- src/main/java/edu/cnm/deepdive/galleryservice/model/dao/GalleryRepository.java
+++ src/main/java/edu/cnm/deepdive/galleryservice/model/dao/GalleryRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GalleryRepository extends CrudRepository<Gallery, UUID> {
 
+  Iterable<Gallery> getAllByOrderByTitleAsc();
+
 }
